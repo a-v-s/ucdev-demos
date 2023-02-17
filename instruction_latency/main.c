93e99040fac029c4d7215cd1a4d1994af17f56b1
André van Schoubroeck <andre@blaatschaap.be>
2023-02-17 18:45:07 +0100

    Update identification code

--- instruction_latency/main.c
+++ instruction_latency/main.c
@@ -92,6 +92,7 @@ void run_test(void){
 
 int main(void){
 	__disable_irq(); // just to make sure interrupt don't mess with our timing
+	void * test = malloc(16);
 	puts(
 "----------------------------------------------------------------------------"
 	);
@@ -123,3 +124,5 @@ int main(void){
 
 	while (1);
 }
+
+//void _init(void) {}
--- todo/32f_testing/identify.c
+++ todo/32f_testing/identify.c
@@ -163,74 +163,200 @@ char * mcuid(){
 #elif defined __ARM_EABI__
 #include "arm_cpuid.h"
 
-char * mcuid() {
+char* parse_32f103_bootrom() {
+	// System ROM starts at 0x1FFFF000
+	// This contains some uart bootloader
+	// at 0x1FFFF7E0 sits the flash size
+	// at 0x1FFFF7E8 sits the UUID
+	// Will calculate the CRC up to 0x1FFFF400
+	// It seems some clones have copied ST's bootloader
+	// That is nasty
 
-	intptr_t ROMTABLE = (intptr_t) (0xE00FF000);
-	romtable_id_t *rid = (romtable_id_t*) (ROMTABLE | 0xFD0);
-	romtable_pid_t romtable_pid = extract_romtable_pid(rid);
-
-	if (romtable_pid.jep106_used) {
-		if (romtable_pid.identity_code == 32
-				&& romtable_pid.continuation_code == 0) {
-			return  "STM32F103";
+	char *prob = "Unknown";
+	__HAL_RCC_CRC_CLK_DISABLE();
+	__HAL_RCC_CRC_CLK_ENABLE();
+	CRC_HandleTypeDef crc_handle;
+	crc_handle.Instance = CRC;
+	HAL_CRC_Init(&crc_handle);
+	// HAL_CRC_Accumulate length is in 32-bit words rather then 8-bit bytes
+	uint32_t crc32 = HAL_CRC_Accumulate(&crc_handle, 0x1FFFF000, 0x100);
+	switch (crc32) {
+	case 0xda6104d0:
+		prob = "STM32F103x6";
+		break;
+	case 0x27377129:
+		prob = "STM32F103xB";
+		// STM32, CS32, APM32
+		if (0x0CF300FF == (*(int32_t*) (0x1FFFF7d0))) {
+			// Seeing 0xFF 0xFF 0xFF 0xFF on STM
+			// Seeing 0xFF 0x00 0xF3 0x0C on APM
+			prob = "APM32 (ApexMic)";
 		}
-		if (romtable_pid.identity_code == 81
-				&& romtable_pid.continuation_code == 7) {
-			return  "GD32F103";
-		}
-		if (romtable_pid.identity_code == 59
-				&& romtable_pid.continuation_code == 4) {
-
-			switch((*(uint32_t*)(0x1FFFF7d0))) {
-			case 0x0CF300FF:
-				return  "APM32F103";
-				break;
-			case 0xFFFFFFFF:
-				switch((*(uint32_t*)(0x1FFFF000))) {
-				case 0x200001fc:
-					return  "CS32F103";
-					break;
-				case 0x20000910:
-					return  "CH32F103";
-					break;
-				}
-				break;
-			}
-
-
-		}
-	} else {
-		// JEP106 not used. Legacy ASCII values are used. This should not be used
-		// on new products. And this note was written in the ADI v5 specs.
-		// The Only value I've been able to find is 0x41 for ARM.
-
-		// The identity/contiuation code are not filled acoording the JEP106
-		// According to speds, this is the legacy idenitification where
-		// the Identity Code contains an ASCII value. On the HK32 we read
-		// JEP106 = false / Identity = 0x55 / Continuation = 5
-		// 0x55 corresponds with 'U'. This looks like this could be an ASCII Identifier.
-		// However, if ASCII IDs are used, the expected Continuation would be 0, as
-		// this field is "reserved, read as zero" when legacy ASCII IDs are used.
-
-		// Even though these values are violating the specs, we can use
-		// JEP106 = false, ID = 0x55, Cont = 5 to detect HK32.
-
-		if (romtable_pid.identity_code == 0x55
-				&& romtable_pid.continuation_code == 5) return  "HK32F103";
+		break;
+	case 0x1527d032:
+		// GD32F101C6
+		// GD32F103CB
+		prob = "GD32F10x";
+		break;
+	case 0x52d42adb:
+		prob = "HK32";
+		break;
+	case 0xdcbd2235:
+		prob = "CH32";
+		break;
 	}
-
-	return "Unkown";
+	return prob;
 
 }
 
-bool is_fpu_present(){
+#define BOOTROM_STM32F103x6 0xda6104d0
+#define BOOTROM_STM32F103xB 0x27377129
+#define BOOTROM_GD32F10x 	0x1527d032
+#define BOOTROM_HK32 		0x52d42adb
+#define BOOTROM_CH32 		0xdcbd2235
+#define BOOTROM_APM32 		0x7be2315b
+#define BOOTROM_FCM32		0xd000a3e2
+
+// RX32 and MM32 give "random" results
+// on the CRC calculation as they appear
+// to have inconsequent behaviour reading
+// from BOOTROM area? There is something else
+// at these addresses.
+//#define BOOTROM_RX32		0x6e7ee3de
+//#define BOOTROM_MM32		0x610aa470
+
+const char* mcuid() {
+
+	intptr_t ROMTABLE = (intptr_t)(0xE00FF000);
+	romtable_id_t *rid = (romtable_id_t*) (ROMTABLE | 0xFD0);
+	romtable_pid_t romtable_pid = extract_romtable_pid(rid);
+
+	// System ROM starts at 0x1FFFF000
+	// This contains some uart bootloader
+	// at 0x1FFFF7E0 sits the flash size
+	// at 0x1FFFF7E8 sits the UUID
+	// Will calculate the CRC up to 0x1FFFF400
+	// It seems some clones have copied ST's bootloader
+	// HAL_CRC_Accumulate length is in 32-bit words rather then 8-bit bytes
+	// Thus 0x100 is given to calculate up to 0x1FFFF400
+
+	__DSB();
+	__HAL_RCC_CRC_CLK_DISABLE();
+	__HAL_RCC_CRC_CLK_ENABLE();
+
+	CRC_HandleTypeDef crc_handle;
+	crc_handle.Instance = CRC;
+	HAL_CRC_Init(&crc_handle);
+	uint32_t bootrom_crc32 = HAL_CRC_Accumulate(&crc_handle, 0x1FFFF000, 0x100);
+
+	cpuid_t *cpuid = (cpuid_t*) (&SCB->CPUID);
+	if ((cpuid->PartNo & (0b11 << 10)) == (0b11 << 10)) {
+		// Cortex family
+		if ((cpuid->PartNo & (0b10 << 4)) == (0b10 << 4)) {
+			// Cortex M
+			int m = cpuid->PartNo & 0xF;
+			int r = cpuid->Variant;
+			int p = cpuid->Revision;
+
+			if (romtable_pid.jep106_used) {
+				if (romtable_pid.identity_code == 32
+						&& romtable_pid.continuation_code == 0) {
+					if (m == 3 && r == 1 && p == 1) {
+						switch (bootrom_crc32) {
+						case BOOTROM_STM32F103xB:
+							return "STM32F103xB (DIE410)";
+						case BOOTROM_STM32F103x6:
+							return "STM32F103x6 (DIE412)";
+						default:
+							return "Unknown";
+						}
+					} else if (m == 4&& r == 0 && p == 1 &&
+					bootrom_crc32 == BOOTROM_FCM32) {
+						return "FCM32";
+					} else if (m == 3 && r == 2 && p == 1) {
+						// Reading into RX32 BOOTROM appears to
+						// read ramdom content. Looking at bootrom CRC
+						// or read from memory location 0x1FFFF7d0
+						// cannot be used to identify. Only going by
+						// a Cortex m3r1p1 with STM in the romtable is
+						// little to go by
+						return "Maybe RX32";
+					}
+				}
+				if (romtable_pid.identity_code == 81
+						&& romtable_pid.continuation_code == 7) {
+					return "GD32F103";
+				}
+				if (romtable_pid.identity_code == 59
+						&& romtable_pid.continuation_code == 4) {
+
+					switch (bootrom_crc32) {
+
+					case BOOTROM_APM32:
+						return "APM32F103 (Geehy)";
+					case BOOTROM_CH32:
+						return "CH32";
+					case BOOTROM_STM32F103xB:
+						// THey've copied STM's bootrom!!!!
+						// On location 0x1FFFF7d0
+						// Seeing 0xFF 0xFF 0xFF 0xFF on STM
+						// Seeing 0xFF 0x00 0xF3 0x0C on APM
+
+						switch ((*(int32_t*) (0x1FFFF7d0))) {
+						case 0xFFFFFFFF:
+							return "CS32";
+						case 0x0CF300FF:
+							return "APM32 (ApexMic)";
+						default:
+							return "Unknown";
+						}
+
+					default:
+						return "Unknown";
+
+					}
+				}
+			} else {
+				// JEP106 not used. Legacy ASCII values are used. This should not be used
+				// on new products. And this note was written in the ADI v5 specs.
+				// The Only value I've been able to find is 0x41 for ARM.
+
+				// The identity/continuation code are not filled according the JEP106
+				// According to specs, this is the legacy identification where
+				// the Identity Code contains an ASCII value. On the HK32 we read
+				// JEP106 = false / Identity = 0x55 / Continuation = 5
+				// 0x55 corresponds with 'U'. This looks like this could be an ASCII Identifier.
+				// However, if ASCII IDs are used, the expected Continuation would be 0, as
+				// this field is "reserved, read as zero" when legacy ASCII IDs are used.
+
+				// Even though these values are violating the specs, we can use
+				// JEP106 = false, ID = 0x55, Cont = 5 to detect HK32.
+
+				if (romtable_pid.identity_code == 0x55
+						&& romtable_pid.continuation_code == 5)
+					return "HK32F103";
+
+				if (romtable_pid.identity_code == 0x00
+						&& romtable_pid.continuation_code == 0)
+					return "MH32F103/Air32F103";
+			}
+		} else {
+			// Not Cortex-M
+		}
+	} else {
+		// Not Cortex
+	}
+	return "Unknown";
+
+}
+
+bool is_fpu_present() {
 	uint32_t initial_val = SCB->CPACR;
-	uint32_t enable_fpu =(0b1111 << 20);
+	uint32_t enable_fpu = (0b1111 << 20);
 	SCB->CPACR |= enable_fpu;
 	bool fpu_present = (SCB->CPACR & enable_fpu);
 	SCB->CPACR = initial_val;
 }
-
 
 #else
 #error "unsupported architecture"
